7949b8dfc8487adca53873592e510b12b1ea0a82
Kyle Rusby <8283338+RusbyProgramming@users.noreply.github.com>
2017-10-27 15:13:57 +0100

    Updated version
    New features:
    - Add words
    - Read words from file
    - Remove words
    - Checks if word is selected in datagrid

---
+++ wordsearch/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace wordsearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wordSearch = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lstWords = new System.Windows.Forms.ListBox();
+            this.lblWords = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAddWord = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.wordSearch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wordSearch
+            // 
+            this.wordSearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.wordSearch.Location = new System.Drawing.Point(13, 4);
+            this.wordSearch.Name = "wordSearch";
+            this.wordSearch.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.wordSearch.Size = new System.Drawing.Size(1042, 249);
+            this.wordSearch.TabIndex = 0;
+            this.wordSearch.MultiSelectChanged += new System.EventHandler(this.selectionChanged);
+            this.wordSearch.SelectionChanged += new System.EventHandler(this.selectionChanged);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(12, 259);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Location = new System.Drawing.Point(106, 264);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(89, 13);
+            this.lblWord.TabIndex = 2;
+            this.lblWord.Text = "Word to look for: ";
+            // 
+            // lstWords
+            // 
+            this.lstWords.FormattingEnabled = true;
+            this.lstWords.Location = new System.Drawing.Point(12, 309);
+            this.lstWords.Name = "lstWords";
+            this.lstWords.Size = new System.Drawing.Size(183, 134);
+            this.lstWords.TabIndex = 3;
+            // 
+            // lblWords
+            // 
+            this.lblWords.AutoSize = true;
+            this.lblWords.Location = new System.Drawing.Point(79, 293);
+            this.lblWords.Name = "lblWords";
+            this.lblWords.Size = new System.Drawing.Size(38, 13);
+            this.lblWords.TabIndex = 4;
+            this.lblWords.Text = "Words";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(201, 340);
+            this.txtInput.MaxLength = 10;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(116, 20);
+            this.txtInput.TabIndex = 5;
+            // 
+            // btnAddWord
+            // 
+            this.btnAddWord.Location = new System.Drawing.Point(201, 309);
+            this.btnAddWord.Name = "btnAddWord";
+            this.btnAddWord.Size = new System.Drawing.Size(116, 23);
+            this.btnAddWord.TabIndex = 6;
+            this.btnAddWord.Text = "Add";
+            this.btnAddWord.UseVisualStyleBackColor = true;
+            this.btnAddWord.Click += new System.EventHandler(this.btnAddWord_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(201, 420);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(116, 23);
+            this.btnRemove.TabIndex = 7;
+            this.btnRemove.Text = "Remove selected";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(201, 366);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(116, 23);
+            this.btnReadFile.TabIndex = 8;
+            this.btnReadFile.Text = "Load from file";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(476, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Found word?:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 455);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAddWord);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblWords);
+            this.Controls.Add(this.lstWords);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.wordSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Word Search Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wordSearch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView wordSearch;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.ListBox lstWords;
+        private System.Windows.Forms.Label lblWords;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAddWord;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ wordsearch/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wordsearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.doWordSearch();
+        }
+
+        private bool tooLong(int direction, Point wordStart, int wordLength)
+        {
+            if (direction == 1) // Word going up
+            {
+                if ((wordStart.Y - wordLength) < 0)
+                    return true;
+            }
+            else if (direction == 2) // Word going diagonal up-right
+            {
+                if ((wordStart.Y - wordLength) < 0 || (wordStart.X + wordLength) > 10)
+                    return true;
+            }
+            else if (direction == 3) // Word going right 
+            {
+                if ((wordStart.X + wordLength) > 10)
+                    return true;
+            }
+            else if (direction == 4) // Word going diagonal down-right
+            {
+                if ((wordStart.Y + wordLength) > 10 || (wordStart.X + wordLength) > 10)
+                    return true;
+            }
+            else if (direction == 5) // Word going down
+            {
+                if ((wordStart.Y + wordLength) > 10)
+                    return true;
+            }
+            else if (direction == 6) // Word going diagonal down-left
+            {
+                if ((wordStart.Y + wordLength) > 10 || (wordStart.X - wordLength) < 0)
+                    return true;
+            }
+            else if (direction == 7) // Word going Left
+            {
+                if ((wordStart.X - wordLength) < 0)
+                    return true;
+            }
+            else if (direction == 8) // Word going diagonal up left
+            {
+                if ((wordStart.Y - wordLength) < 0 || (wordStart.X - wordLength) < 0)
+                    return true;
+            }
+            return false;
+        }
+
+        // Method used to generate random numbers:
+        private object stateLock = new object();
+        private Random rnd = new Random();
+        private int getRand(int num, int num2)
+        {
+            lock (stateLock)
+                return rnd.Next(num, num2);
+        }
+
+        // Method used to reload the word search
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            this.doWordSearch();
+        }
+
+        private void doWordSearch()
+        {
+            this.wordSearch.Rows.Clear();
+            this.wordSearch.Columns.Clear();
+
+            this.wordSearch.AllowUserToAddRows = false;
+
+            // Setting up the word search by adding 10 columns and 10 rows:
+            for (int i = 0; i < 10; i++)
+            {
+                string colNum = "col" + (i + 1);
+                this.wordSearch.Columns.Add(colNum, colNum);
+            }
+            this.wordSearch.Rows.Add(10);
+
+            // Setting up arrays of pre-installed words for the user to find + an alphabet array:#
+            this.locationsOfLetters = new List<Point>();
+            string[] words = this.lstWords.Items.OfType<string>().ToArray();
+
+            if (words.Length == 0)
+                return;
+
+            string word = words[getRand(0, words.Length)];
+
+            // Tell the user what word they are looking for:
+            this.lblWord.Text = "Word to look for: " + word;
+
+            // Setting up directions for the word to span:
+            int[] directions = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int direction = directions[getRand(0, directions.Length)];
+
+            // Get a location for the word to start at:
+            Point wordStart;
+
+            do
+            {
+                wordStart = new Point(getRand(0, 10), getRand(0, 10));
+            } while (this.tooLong(direction, wordStart, word.Length));
+
+            // Iterate over the chosen word and lay it out in the randomly chosen direction:
+            Point currentPoint = new Point(wordStart.X, wordStart.Y);
+            for (int i = 0; i < word.Length; i++)
+            {
+                this.wordSearch[currentPoint.X, currentPoint.Y].Value = word[i];
+                this.locationsOfLetters.Add(new Point(currentPoint.X, currentPoint.Y));
+                this.wordSearch[currentPoint.X, currentPoint.Y].Style.BackColor = Color.LightGray;
+                switch (direction)
+                {
+                    case 1:
+                        currentPoint.Y = (currentPoint.Y - 1);
+                        break;
+
+                    case 2:
+                        currentPoint.Y = (currentPoint.Y - 1);
+                        currentPoint.X = (currentPoint.X + 1);
+                        break;
+
+                    case 3:
+                        currentPoint.X = (currentPoint.X + 1);
+                        break;
+                    case 4:
+                        currentPoint.Y = (currentPoint.Y + 1);
+                        currentPoint.X = (currentPoint.X + 1);
+                        break;
+                    case 5:
+                        currentPoint.Y = (currentPoint.Y + 1);
+                        break;
+                    case 6:
+                        currentPoint.Y = (currentPoint.Y + 1);
+                        currentPoint.X = (currentPoint.X - 1);
+                        break;
+                    case 7:
+                        currentPoint.X = (currentPoint.X - 1);
+                        break;
+                    case 8:
+                        currentPoint.Y = (currentPoint.Y - 1);
+                        currentPoint.X = (currentPoint.X - 1);
+                        break;
+                }
+            }
+
+
+            // For loops used to fill in remaining gaps after the word has been layed out inside the grid:
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (this.wordSearch[i, j].Value == null)
+                    {
+                        this.wordSearch[i, j].Value = (char)(('a') + getRand(0, 26));
+                    }
+                }
+            }
+        }
+
+        private void btnAddWord_Click(object sender, EventArgs e)
+        {
+            string wordEdited = this.txtInput.Text.Replace(" ", "");
+            if (wordEdited.Length > 0 && wordEdited.Length <= 10 && !this.lstWords.Items.Contains(wordEdited))
+            {
+                this.lstWords.Items.Add(wordEdited);
+            }
+            else
+            {
+                MessageBox.Show("Word is too long, short, or has already been entered");
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            this.lstWords.Items.Remove(this.lstWords.SelectedItem);
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.openFileDialog.ShowDialog(); 
+            if (result == DialogResult.OK) 
+            {
+                string file = this.openFileDialog.FileName;
+                try
+                {
+                    foreach (string word in File.ReadAllLines(file))
+                        this.lstWords.Items.Add(word);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private List<Point> locationsOfLetters;
+        private void selectionChanged(object sender, EventArgs e)
+        {
+            DataGridViewSelectedCellCollection cells = this.wordSearch.SelectedCells;
+
+            if (cells.Count <= 0 || this.locationsOfLetters == null)
+                return;
+
+            List<Point> pointsSelected = new List<Point>();
+            foreach (DataGridViewTextBoxCell cell in cells)
+            {
+                pointsSelected.Add(new Point(cell.ColumnIndex, cell.RowIndex));
+            }
+            if (pointsSelected.All(this.locationsOfLetters.Contains) && pointsSelected.Count == this.locationsOfLetters.Count)
+                this.label1.Text = "Found word";
+            else
+                this.label1.Text = "Not found word yet";
+        }
+    }
+}
